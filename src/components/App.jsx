3b277b66abddd76888019d82f13e9ca0543a1a2e
李沛秦(Peiqin Li) <peiqin@cratebind.com>
2017-08-25 11:41:27 -0500

    set state for menu

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,39 @@ import React from 'react';
 import Header from './Header'
 import Order from './Order'
 import Inventory from './Inventory'
+import sampleFishes from '../sample-fishes'
+
 
 class App extends React.Component {
+  constructor(){
+    // once we call super, we can call (this) indicated App
+    super()
+    // get initial state
+    this.state = {
+      fishes: {},
+      order: {}
+    }
+
+    this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
+  }
+
+  addFish(fish) {
+    // update state
+    // ... means take every item from object and spread into new object
+    const fishes = {...this.state.fishes}
+    // add new fish
+    const timestamp = Date.now()
+    fishes[`fish-${timestamp}`] = fish;
+    // set state
+    this.setState({ fishes })
+  }
+
+  loadSamples(){
+    this.setState({
+      fishes: sampleFishes
+    })
+  }
   render() {
     return (
       <div className="catch-of-the-day">
@@ -13,7 +44,9 @@ class App extends React.Component {
           ></Header>
         </div>
         <Order></Order>
-        <Inventory></Inventory>
+        <Inventory
+          addFish={this.addFish}
+          loadSamples={this.loadSamples} />
       </div>
     )
   }
--- src/components/StorePicker.jsx
+++ src/components/StorePicker.jsx
@@ -3,17 +3,39 @@ import React from "react";
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  // constructor() {
+    // super();
+    // this inside the () indicates storePicker
+    // this.goToStore = this.goToStore.bind(this)
+  // }
+  goToStore(event){
+    event.preventDefault();
+    // grab the test from the box
+    this.context.router.transitionTo(`/store/${this.storeInput.value}`)
+    // second we are going to transition from / to /store/:storeId
+
+  }
   render() {
+
     return (
-      <form className="store-selector">
+      // e indicates storePicker
+      // another way is this.goToStore.bind(this)
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please Enter A Store</h2>
         <input type="text" required placeholder="Store Name"
           defaultValue={getFunName()}
+          ref={(input) => { this.storeInput = input}}
         />
+          {
+            // ref means $("input"), DOM element
+          }
         <button type="submit">Visit Store -></button>
       </form>
     )
   }
 }
 
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
+}
 export default StorePicker;
