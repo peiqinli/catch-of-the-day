806946b1c9a65c546b54c209aa5814dd88d700ac
李沛秦(Peiqin Li) <peiqin@cratebind.com>
2017-09-13 22:04:28 -0500

    finish add to order function

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -3,12 +3,24 @@ import {formatPrice} from '../helpers'
 
 class Order extends React.Component {
 
+  constructor() {
+    super();
+    this.renderOrder = this.renderOrder.bind(this)
+  }
+
   renderOrder(key) {
-    const fish = this.props.fished[key]
+    const fish = this.props.fishes[key]
     const count = this.props.order[key]
-    if (!fish || fish.status == "unavailable"){
+    if (!fish || fish.status === "unavailable"){
       return <li key={key}> Sorry, {fish ? fish.name : "fish"} is no longer available</li>
     }
+
+    return (
+      <li key={key}>
+        <span>{count}lbs {fish.name}</span>
+        <span className="price">{formatPrice(count * fish.price)}</span>
+      </li>
+    )
   }
 
 
@@ -17,7 +29,7 @@ class Order extends React.Component {
     const total = orderIds.reduce((prevTotal, key) => {
       const fish = this.props.fishes[key]
       const count = this.props.order[key]
-      const isAvailable = fish && fish.status == 'available'
+      const isAvailable = fish && fish.status === 'available'
       if(isAvailable) {
         return prevTotal + (count * fish.price || 0)
       }
